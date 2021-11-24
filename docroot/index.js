561c6acc7e08f34555f128242b2584dcdff17730
Scott Windon <swindon.hd@gmail.com>
2021-11-24 10:30:46 +0000

    Update index.js

--- docroot/index.js
+++ docroot/index.js
@@ -17,16 +17,16 @@ function app() {
             this.update_dims();
         },
         update_calc: function() {
-            var ppm_value = document.querySelector('[name="ppm"]').value;
+            var ppm_value = document.querySelector('[data-name="ppm"]').value;
             var wt_factor = 1;
             var vol_factor = 1.0;
-            var total_vol = vol_factor * document.querySelector('[name="brine_amt"]').value + wt_factor * document.querySelector('[name="meat_amt"]').value;
+            var total_vol = vol_factor * document.querySelector('[data-name="brine_amt"]').value + wt_factor * document.querySelector('[data-name="meat_amt"]').value;
             var gms_no2 = total_vol * 1000 * ppm_value / 1000000 / 0.0625;
             this.calc_salt = (gms_no2).toFixed(1) + " grams";
         },
         update_dims: function() {
             var t_factor = 1 / 2.54;
-            var tdim = t_factor * document.querySelector('[name="thick_ness"]').value;
+            var tdim = t_factor * document.querySelector('[data-name="thick_ness"]').value;
             var curetime = 1.35 * tdim * tdim;
             if (document.getElementById("shape_flat").checked) {
                 this.calc_days = (curetime).toFixed(1) + " days"
